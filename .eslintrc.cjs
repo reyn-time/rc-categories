0b10f042e2b4910d369b82d1be65f8a6037a6a4f
Ryan Lau <ryanlau394@gmail.com>
2024-04-03 21:39:14 +0100

    Auto Prettier+ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: {
